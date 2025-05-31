db0c79000787418b89719515404901b9d55064c4
Dat Nguyen <datnguyen2112003@gmail.com>
2025-06-01 05:47:05 +0700

    add context

---
+++ src/context/sll/headRefContext.jsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const HeadRefContext = createContext(null);
+export const HeadLineContext = createContext(null);
+export const TailLineXContext = createContext(null);
+export const TailRefContext = createContext(null);
+export const TailLineYContext = createContext(null);
+export const HeadRefTextContext = createContext(null);
+export const TailRefTextContext = createContext(null);
+
